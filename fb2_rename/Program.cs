efd4631c85c0aadd32d71f3fdd563a5dc5744681
fryn3 <fryn3@mail.ru>
2017-03-03 21:53:51 +0300

    Версия 0.2 Исправленно: Поиск во вложениях Добавленно: Вытаскивает файлы из zip архивов. Записывает книги по папкам, по фамилии автора.

--- fb2_rename/Program.cs
+++ fb2_rename/Program.cs
@@ -1,83 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Xml;
-using System.Xml.Linq;
+using System.IO.Compression;
 
-namespace fb2_rename
+namespace Find_fb2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(@"Вставьте путь к папкам с книгами. Например: C:\Users\q\Desktop\book\");
-            string address;
-            address = Console.ReadLine();
-            if (!address.EndsWith("\\"))
-                address += "\\";
-            string new_directory = @"ru_name\";
-            
-            DirectoryInfo dir = new DirectoryInfo(address);
-            foreach (var item in dir.GetDirectories())  // обход по каталогам
-            {
-                foreach (var it in item.GetFiles())     // обход по файлам
-                {
-                    //Console.Write(it.Name + " = ");
-                    XmlReader xmlReader = new XmlTextReader(it.FullName);
-                    do xmlReader.Read(); //Читаем документ
-                    while (xmlReader.Name != "book-title"); //Пока не найдем узел book-title
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(@"Вставьте путь к папкам с книгами (к *.fb2 или *.zip). Например: C:\Users\q\Desktop\book\");
+			string address;
+			address = Console.ReadLine();
+			if (!address.EndsWith("\\"))
+				address += "\\";
+			FileInfo[] array_zip_file = FindFile(address, "*.zip");
+			string temp_dir = "__temp_fb2_rename\\";     // используется для вытаскивания файлов из zip. в конце удаляется.
+			DirectoryInfo dir = new DirectoryInfo(address);
+			if (!dir.Exists)
+			{
+				Console.WriteLine("Не существует такого пути!");
+				return;
+			}
+			if (!Directory.Exists(address + temp_dir))
+				Directory.CreateDirectory(address + temp_dir);  // создание папки __temp_fb2_rename
+			foreach (FileInfo zip in array_zip_file)
+			{
+				if (zip.FullName.EndsWith(".zip"))  // вторая проверка. но пусть будет
+				{
+					using (ZipArchive archive = ZipFile.OpenRead(zip.FullName))
+					{
+						try
+						{
+							archive.ExtractToDirectory(address + temp_dir); // извлечение всего архива
+						}
+						catch
+						{
+							continue;
+						}
+					}
+				}
+			}
+			FileInfo[] array_fb2_file = FindFile(address, "*.fb2");
+			string new_directory = @"ru_name\";
+			if (!Directory.Exists(address + new_directory))
+				Directory.CreateDirectory(address + new_directory);  // создание папки ru_name
+			foreach (FileInfo fb2 in array_fb2_file)
+			{
+				if (fb2.FullName.EndsWith(".fb2"))
+				{
+					using (XmlReader xmlReader = new XmlTextReader(fb2.FullName))
+					{
+						try
+						{
+							string last_name = "", book_title = "";
+							while (xmlReader.Name != "last-name" && xmlReader.Read()) ;  //Пока не найдем узел book-title или конец файла
+							if (xmlReader.Name == "last-name")
+							{
+								xmlReader.Read();                   //Читаем узел last-name
+								last_name = xmlReader.Value;
+								last_name = last_name.Trim();
+								var matchs = Regex.Matches(last_name, @"[\w.,!()&#№ \-]*");
+								last_name = "";
+								foreach (Match mat in matchs)
+									last_name += mat.Value;
+							}
+							else
+								Console.WriteLine("Не нашел фамилию автора в файле: " + fb2.FullName);
+							while (xmlReader.Name != "book-title" && xmlReader.Read()) ;  //Пока не найдем узел book-title или конец файла
+							if (xmlReader.Name == "book-title")
+							{
+								xmlReader.Read();                   //Читаем узел book-title
+								book_title = xmlReader.Value;
+								book_title = book_title.Trim();
+								var matchs = Regex.Matches(book_title, @"[\w.,!()&#№ \-]*");
+								book_title = "";
+								foreach (Match mat in matchs)
+									book_title += mat.Value;
+								Console.WriteLine(fb2.Name + "\t->\t" + last_name.ToUpper() + "\\" + book_title);
+								if (!System.IO.Directory.Exists(address + new_directory + last_name.ToUpper() + @"\"))
+								{
+									System.IO.Directory.CreateDirectory(address + new_directory + last_name.ToUpper() + @"\");
+								}
+								File.Copy(fb2.FullName, address + new_directory + last_name.ToUpper() + @"\" + book_title + ".fb2", true);
+							}
+							else
+								Console.WriteLine("Не нашел название книги в файле: " + fb2.FullName);
 
-                    xmlReader.Read();                   //Читаем узел book-title
-                    string book_title = xmlReader.Value;
-                    book_title =  book_title.Trim();
-                    var matchs = Regex.Matches(book_title, @"[\w.,!()&#№ \-]*");
-                    book_title = "";
-                    foreach(Match mat in matchs)
-                        book_title += mat.Value;
-                    Console.WriteLine(item.Name + "\t->\t" + book_title);
-                    if (!System.IO.Directory.Exists(address + new_directory))
-                    {
-                        System.IO.Directory.CreateDirectory(address + new_directory);
-                    }
-                    if (!System.IO.Directory.Exists(address + new_directory + item.Name + @"\"))
-                    {
-                        System.IO.Directory.CreateDirectory(address + new_directory + item.Name + @"\");
-                    }
-                    File.Copy(it.FullName, address + new_directory + item.Name + @"\" + book_title + ".fb2", true);
-                }
-            }
-            foreach (var it in dir.GetFiles())     // обход по файлам
-            {
-                //Console.Write(it.Name + " = ");
-                XmlReader xmlReader = new XmlTextReader(it.FullName);
-                do xmlReader.Read(); //Читаем документ
-                while (xmlReader.Name != "book-title"); //Пока не найдем узел book-title
+						}
+						catch
+						{
+							Console.WriteLine("Ошибка в файле: " + fb2.FullName);
+							continue;
+						}
+					}
+				}
+			}
+			if (Directory.Exists(address + temp_dir))
+				Directory.Delete(address + temp_dir, true);
+		}
+		static FileInfo[] FindFile(string address, string filename)
+		{
+			DirectoryInfo dir = new DirectoryInfo(address);
+			FileInfo[] file_inf = null;
+			if (dir.Exists)
+			{
+				file_inf = dir.GetFiles(filename);
 
-                xmlReader.Read();                   //Читаем узел book-title
-                string book_title = xmlReader.Value;
-                book_title = book_title.Trim();
-                var matchs = Regex.Matches(book_title, @"[\w.,!()&#№ \-]*");
-                book_title = "";
-                foreach (Match mat in matchs)
-                    book_title += mat.Value;
-                Console.WriteLine(book_title);
-                if (!System.IO.Directory.Exists(address + new_directory))
-                {
-                    System.IO.Directory.CreateDirectory(address + new_directory);
-                }
-                if (!System.IO.Directory.Exists(address + new_directory))
-                {
-                    System.IO.Directory.CreateDirectory(address + new_directory);
-                }
-                File.Copy(it.FullName, address + new_directory + book_title + ".fb2", true);
-            }
-            Console.WriteLine();
-            Console.WriteLine(@"Результат работы в папке: " + address + new_directory);
-            Console.ReadLine();
-        }
-    }
+				foreach (DirectoryInfo subdir in dir.GetDirectories())
+					__FindFile(subdir, filename, ref file_inf);
+			}
+			return file_inf;
+		}
+		static void __FindFile(DirectoryInfo dir, string filename, ref FileInfo[] file_inf)
+		{
+			if (dir.Exists)
+			{
+				FileInfo[] file_inf2 = dir.GetFiles(filename);
+				file_inf = file_inf.Concat(file_inf2).ToArray();
+
+				foreach (DirectoryInfo subdir in dir.GetDirectories())
+					__FindFile(subdir, filename, ref file_inf);
+			}
+		}
+
+	}
 }
